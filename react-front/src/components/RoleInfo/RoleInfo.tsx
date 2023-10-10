e39b694c3b8acdc224532f0512ef01ea4ae751eb
michael <michael_hll@hotmail.com>
2023-10-10 21:22:00 +0800

    add user role maintenance logic

--- react-front/src/components/RoleInfo/RoleInfo.tsx
+++ react-front/src/components/RoleInfo/RoleInfo.tsx
@@ -1,5 +1,7 @@
+import AddIcon from '@mui/icons-material/Add';
 import ClearIcon from '@mui/icons-material/Clear';
-import { Box, IconButton, Typography } from "@mui/material";
+import { Box, IconButton, MenuItem, Select, SelectChangeEvent, Theme, Typography, useTheme } from "@mui/material";
+import { useState } from 'react';
 import { RoleCodes } from '../../store/enum/RoleCodes';
 
 interface RoleInfoProps {
@@ -7,10 +9,46 @@ interface RoleInfoProps {
   name: string;
   description: string;
   roles: string[];
+  allRoles: string[];
   deleteHandler: (id: string, code: string) => void;
+  addHandler: (id: string, codes: string[]) => void;
 }
 
-function RoleInfo({ id, name, description, roles, deleteHandler }: RoleInfoProps) {
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 2;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+function getStyles(code: string, codes: string[], theme: Theme) {
+  return {
+    fontWeight:
+      codes.indexOf(code) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+    fontSize: '14px',
+  };
+}
+
+function RoleInfo({ id, name, description, roles, allRoles, deleteHandler, addHandler}: RoleInfoProps) {
+
+  const theme = useTheme();
+
+  const [roleCodes, setRoleCodes] = useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof roleCodes>) => {
+    const {
+      target: { value },
+    } = event;
+    setRoleCodes(
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
 
   return (
     <Box sx={{
@@ -21,10 +59,11 @@ function RoleInfo({ id, name, description, roles, deleteHandler }: RoleInfoProps
       <Box sx={{
         display: 'flex',
         width: '100%',
+        backgroundColor: '#1c7ed6',
         border: 1,
         borderBottomColor: 'gray',
       }}>
-        <Typography variant="caption"
+        <Typography variant="body1"
           sx={{
             margin: '0px 0px 0px 8px',
             flex: '1 0 50%',
@@ -32,7 +71,7 @@ function RoleInfo({ id, name, description, roles, deleteHandler }: RoleInfoProps
         >
           {name}
         </Typography>
-        <Typography variant="caption"
+        <Typography variant="body1"
           sx={{
             margin: '0px 0px 0px 8px',
             flex: '1 0 50%',
@@ -51,7 +90,7 @@ function RoleInfo({ id, name, description, roles, deleteHandler }: RoleInfoProps
         borderBottomColor: 'gray',
         borderTopColor: 'gray',
       }}>
-        <Typography variant="caption"
+        <Typography variant="body1"
           sx={{
             margin: '0px 0px 0px 8px',
           }}
@@ -68,9 +107,67 @@ function RoleInfo({ id, name, description, roles, deleteHandler }: RoleInfoProps
         borderBottom: 1,
         padding: '2px'
       }}>
-        {roles.map(role => {
-          return <RoleCard key={role} id={id} code={role} deleteHandler={deleteHandler} />
-        })}
+        {/** Cards */}
+        <Box sx={{
+          display: 'flex',
+          flex: 1,
+          marginRight: '12px',
+        }}>
+          {roles.map(role => {
+            return <RoleCard key={role} id={id} code={role} deleteHandler={deleteHandler} />
+          })}
+        </Box>
+        {/** Role dropdown list */}
+        <Box sx={{
+          display: 'flex',
+          width: '256px',
+          marginLeft: 'auto',
+          marginRight: '4px',
+        }}>
+          <Select
+            variant='outlined'
+            multiple
+            value={roleCodes}
+            onChange={handleChange}
+            MenuProps={MenuProps}
+            sx={{
+              width: '100%',
+              height: '28px',
+              fontSize: '16px',
+            }}
+          >
+            {allRoles.map((code) => (
+              <MenuItem
+                key={code}
+                value={code}
+                style={getStyles(code, allRoles, theme)}
+              >
+                {code}
+              </MenuItem>
+            ))}
+          </Select>
+        </Box>
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          float: 'right',
+          marginLeft: 'auto',
+        }}>
+          <IconButton aria-label="add"
+            onClick={() => {
+              addHandler(id, roleCodes);
+              setRoleCodes([]);
+            }}
+            sx={{
+              width: '8px',
+              height: '8px',
+              marginLeft: '2px',
+              marginRight: '2px',
+              border: 0,
+            }}>
+            <AddIcon sx={{ fontSize: '18px' }} />
+          </IconButton>
+        </Box>
       </Box>
     </Box>
   );
@@ -84,12 +181,19 @@ interface RoleCardProps {
 
 function RoleCard({ id, code, deleteHandler }: RoleCardProps) {
   let showDeleteButton = ''
-  if(code === RoleCodes.DEFAULT){
+  if (code === RoleCodes.DEFAULT) {
     showDeleteButton = 'none';
   }
   return (
     <Box
-      sx={{ marginX: '2px', border: 1, borderColor: 'gray'}}
+      sx={{ 
+        display: 'flex',
+        alignItems: 'center',
+        marginX: '2px', 
+        marginY: '0px', 
+        border: 1, 
+        borderColor: 'gray', 
+        borderRadius: '2px', }}
     >
       <Box
         sx={{
@@ -98,18 +202,19 @@ function RoleCard({ id, code, deleteHandler }: RoleCardProps) {
         }}
       >
         <Typography
-          variant='caption'
+          variant='body1'
           align='center'
           sx={{
-            margin: '0px px 0px 0px',
-            padding: '0px 4px 0px 4px',
-            fontSize: '12px',
+            margin: '0px 0px 0px 0px',
+            padding: '0px 1px 0px 1px',
             backgroundColor: 'red',
+            flex: '0 1 auto',
+            borderRadius: '2px',
           }}
         >
           {code}
         </Typography>
-        <IconButton aria-label="delete"                   
+        <IconButton aria-label="delete"
           onClick={() => deleteHandler(id, code)}
           sx={{
             width: '8px',
@@ -117,9 +222,9 @@ function RoleCard({ id, code, deleteHandler }: RoleCardProps) {
             display: showDeleteButton,
             marginLeft: '2px',
             marginRight: '2px',
-            border: 0,            
+            border: 0,
           }}>
-          <ClearIcon sx={{ fontSize: '16px' }} />
+          <ClearIcon sx={{ fontSize: '18px' }} />
         </IconButton>
       </Box>
 
--- react-front/src/views/Role/UserRolesView.tsx
+++ react-front/src/views/Role/UserRolesView.tsx
@@ -3,20 +3,38 @@ import RoleInfo from "../../components/RoleInfo/RoleInfo";
 import { useUserRoles } from "../../hooks/user";
 import { Box } from "@mui/material";
 import { User } from "../../store/interfaces/User";
+import { Role } from "../../store/interfaces/Role";
+import { useRoles } from "../../hooks/role";
+import { RoleCodes } from "../../store/enum/RoleCodes";
 
 function UserRolesView() {
-  const [error, setError] = useState<string>('');
-  const [users, setUsers] = useState<User[]>([]);
+
   const userRolesQuery = useUserRoles((data) => {
     setUsers(data);
   }, (error) => {
     setError(error.message);
   });
+  const rolesQuery = useRoles((data) => {
+    setRoles(data);
+    console.log('roles', data);
+  }, (error) => {
+    setError(error.message);
+    console.log(error);
+  });
+  const [error, setError] = useState<string>('');
+  const [users, setUsers] = useState<User[]>([]);
+  const [roles, setRoles] = useState<Role[]>(rolesQuery.data || []);
 
   function deleteRoleByUserId(id: string, code: string) {
     console.log('will delete: ', id, code);
 
   }
+
+  function addRolesByUserId(id: string, codes: string[]) {
+    console.log('will add: ', id, codes);
+
+  }
+
   return (
     <Box>
       {users.map(user => (
@@ -26,7 +44,9 @@ function UserRolesView() {
           name={user.username!}
           description={`Email: ${user.email} Phone: ${user.cellphone} Country: ${user.country} Address: ${user.address1}`}
           roles={user.roles?.map(role => role.code!) || []}
+          allRoles={roles.map(role => role.code!)}
           deleteHandler={deleteRoleByUserId}
+          addHandler={addRolesByUserId}
         />))}
     </Box>
   );
